e449efd926ef07013f0a1d15ae83f06288f3ef0e
Laura Schooley <laura.schooley3@gmail.com>
2024-11-01 17:43:06 -0500

    fixed seeding issue

--- db/query.sql
+++ db/query.sql
@@ -1,5 +1,4 @@
-SELECT department.department_name AS department, role.title, employee.last_name
-FROM employee
-LEFT JOIN department ON employee.department_id=department.id
+SELECT department.department_name AS department, role.title, employee.last_name FROM employee
 JOIN role ON employee.role_id=role.id
 ORDER BY department.department_name, role.title, employee.last_name;
+LEFT JOIN employee ON manager_id=employee.id
--- db/seeds.sql
+++ db/seeds.sql
@@ -3,25 +3,16 @@ VALUES ('Human Resources'),
        ('Sales'),
        ('Production');
 
-INSERT INTO role (department_name, title)
-VALUES (1, 'Representative'),
-       (2, 'Sales floor Rep'),
-       (1, 'Director of HR'),
-       (3, 'Lead'),
-       (2, 'Sales floor Rep'),
-       (1, 'Representative'),
-       (3, 'Team Member');
+INSERT INTO role (title, salary, department_id)
+VALUES ('Representative', 10000, 1),
+       ('Sales floor Rep', 20000, 2),
+       ('Director of HR', 50000, 1),
+       ('Lead', 5000, 3),
+       ('Team Member', 2000000, 3);
        
-INSERT INTO employee (title, first_name, last_name, role_id, manager_id)
-VALUES (3, John, Smith, 1, NULL),
-        (2, Jane, Doe, 2, 1),
-        (1, Bob, Builder, 3, 1),
-        (3, Sally, Smith, 4, 2),
-        (2, Joe, Dirt, 5, 2),
-        (1, Mary, Jones, 6, 3),
-        (2, Susie, Homemaker, 7, 3),
-        (2, Jack, Attack, 8, 4),
-        (1, Jacob, Black, 9, 4),
-        (3, Harry, Potter, 10, 5),
-        (2, Ron, Weasley, 11, 5),
-        (1, Hermione, Granger, 12, 6);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Doe', 1, 3),
+       ('Jane', 'Doe', 2, 3),
+       ('Jim', 'Doe', 3, 3),
+       ('Jill', 'Doe', 4, 3),
+       ('Jack', 'Doe', 5, 3);
